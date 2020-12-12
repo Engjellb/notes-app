0d24906221f43c4c6d928c5632201e5a3d0d5acd
Engjellb <engjibeselica@gmail.com>
2020-12-12 20:36:34 +0100

    modified mail credentials

--- config/mail.php
+++ config/mail.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'mail_username' => '83702c5a653d2f',
-    'mail_password' => '5c42758f5b591b',
+    'mail_username' => '',
+    'mail_password' => '',
     //smpt fake server to test emails
     'mail_server' => 'smtp.mailtrap.io',
     'mail_port' => 25
